a6438a1f4fcc0180e58c22cd7f6f6d59338cf9da
Denis Veller <denis@Deniss-MacBook-Pro.local>
2018-04-12 16:33:39 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+var server = require('http').createServer(handler);
+var io = require('socket.io')(server);
+var fs = require('fs');
+server.listen(process.env.PORT || process.env.port || 8080);
+
+function handler(req, res) {
+  fs.readFile(__dirname + '/snake.html',
+    function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading snake.html');
+      }
+
+      res.writeHead(200);
+      res.end(data);
+ 
+    });
+}
+var score;
+var username;
+var recordObject = {score, username};
+recordObject.score = 0;
+io.on('connection', function (socket) {
+  io.emit('setRecords', recordObject);
+  socket.on('highScore', function (record) {
+    recordObject = record;
+    io.emit('newRecord', recordObject);
+  });
+});
