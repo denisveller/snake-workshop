686593c3fd1ebabae0202d543c1d2bd5543d8126
Denis Veller <denis.veller@gmail.com>
2018-04-26 20:08:55 -0400

    Removed /files, made /files actually redirect to requisite page.

--- server.js
+++ server.js
@@ -3,24 +3,42 @@ var eventEmitter = new events.EventEmitter();
 var server = require('http').createServer(handler);
 var io = require('socket.io')(server);
 var fs = require('fs');
+var url = require('url');
 server.listen(process.env.PORT || process.env.port || 8080);
 
 function handler(req, res) {
-  fs.readFile(__dirname + '/snake.html',
-    function (err, data) {
-      if (err) {
-        res.writeHead(500);
-        return res.end('Error loading snake.html');
-      }
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname
+  if (filename === "./files/") {
+    fs.readFile(__dirname + '/index.html',
+      function (err, data) {
+        if (err) {
+          res.writeHead(500);
+          return res.end('Error loading flies');
+        }
 
-      res.writeHead(200);
-      res.end(data);
- 
-    });
+        res.writeHead(200);
+        res.end(data);
+
+      });
+
+  } else {
+    fs.readFile(__dirname + '/snake.html',
+      function (err, data) {
+        if (err) {
+          res.writeHead(500);
+          return res.end('Error loading snake.html');
+        }
+
+        res.writeHead(200);
+        res.end(data);
+
+      });
+  }
 }
 var score = 0;
 var username;
-var recordObject = {score, username};
+var recordObject = { score, username };
 recordObject.score = 0;
 io.on('connection', function (socket) {
   io.emit('setRecords', recordObject);
